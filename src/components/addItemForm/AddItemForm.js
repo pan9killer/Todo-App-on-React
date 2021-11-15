73fca36ec955d1ba1cd41f5b3c81023b42fff0cc
pan9killer <pan9killer@gmail.com>
2021-11-15 12:16:25 +0400

    filter ready

--- src/components/addItemForm/AddItemForm.js
+++ src/components/addItemForm/AddItemForm.js
@@ -2,15 +2,39 @@ import React, { Component } from "react";
 import "./AddItemForm.css";
 
 export default class AddItemForm extends Component {
+  state = {
+    label: ''
+  };
+
+  onLabelChange = (e) => {
+    this.setState({
+      label: e.target.value
+    });
+  };
+
+  onSubmit = (e) => {
+    if(this.state.label){
+      e.preventDefault();
+      this.props.addItem(this.state.label)
+      this.setState({
+        label: ''
+      });
+    };
+  };
+
   render(){
-    // const {addItem} = this.props
     return(
-      <div className="item__add__form">
-        <button className="btn btn-outline-secondary"
-          onClick={() => this.props.addItem('jopa')}>
+      <form className="item__add__form d-flex"
+        onSubmit={this.onSubmit}>
+        <input type="text"
+          className="form-control"
+          onChange={this.onLabelChange}
+          placeholder="Enter a new Todo..." 
+          value={this.state.label} />
+        <button className="btn btn-outline-secondary">
           Add Item
           </button>
-      </div>
+      </form>
     )
   }
 }
--- src/components/app/App.js
+++ src/components/app/App.js
@@ -8,14 +8,15 @@ import "./App.css"
 
 export default class App extends Component {
   maxId = 100;
-
   state = {
     todoData: [
       this.createTodoItem('Eat Cake'),
       this.createTodoItem('Make React App'),
       this.createTodoItem('Drink Tea')
-    ]
-  }
+    ],
+    term: '',
+    filter: 'all'
+  };
 
   toggleProperty(arr,id,propName){
     const doneEl = arr.find((el) => el.id === id);
@@ -26,7 +27,7 @@ export default class App extends Component {
       newItem,
       ...arr.slice(idx+1)
     ];
-  }
+  };
 
   createTodoItem(label){
     return {
@@ -34,8 +35,8 @@ export default class App extends Component {
       important: false,
       done: false,
       id: this.maxId++
-    }
-  }
+    };
+  };
 
   addItem = (text) => {
     const newTodoItem = this.createTodoItem(text);
@@ -46,9 +47,9 @@ export default class App extends Component {
       ];
       return {
         todoData: newTodoData
-      }
+      };
     });
-  }
+  };
 
   deleteItem = (id) => {
     this.setState(({ todoData }) => {
@@ -56,28 +57,69 @@ export default class App extends Component {
       const newArray = todoData.filter(el => el.id !== idx.id);
       return{
         todoData: newArray
-      }
-    })
+      };
+    });
   };
 
   onToggleImportant = (id) => {
     this.setState(({ todoData }) => {
       return {
         todoData: this.toggleProperty(todoData, id, 'important')
-      }
-    })
-  }
+      };
+    });
+  };
 
   onToggleDone = (id) => {
     this.setState(({ todoData }) => {
       return {
         todoData: this.toggleProperty(todoData, id, 'done')
-      }
-    })
+      };
+    });
+  };
+
+  showActive = () => {
+    this.setState(({ todoData }) => {
+      const newArray = todoData.filter(el => el.done !== true);
+      return{
+        todoData: newArray
+      };
+    });
+  };
+
+  filter(items, filter){
+    switch(filter){
+      case 'all':
+        return items;
+      case 'active':
+        return items.filter(item => !item.done);
+      case 'done':
+        return items.filter(item => item.done);
+      default:
+        return items;
+    };
   }
 
+  search(items, term){
+    if(term === 0){
+      return items
+    };
+    return items.filter((item) => item.label
+    .toLowerCase()
+    .includes(term.toLowerCase()));
+  };
+
+  onSearchChange = (term) => {
+    this.setState({term});
+  };
+
+  onFilterChange = (filter) => {
+    this.setState({ filter });
+  };
+
   render(){
-    const {todoData} = this.state;
+    const {todoData, term, filter} = this.state;
+    const visibleItems = this.filter(this.search(todoData, term), filter);
+
     const doneCount = todoData.filter(el => el.done).length;
     const toDoCount = todoData.length - doneCount;
 
@@ -85,15 +127,21 @@ export default class App extends Component {
       <div className="todo-app">
         <AppHeader toDo={toDoCount} done={doneCount} />
         <div className="top-panel d-flex">
-          <SearchPanel />
-          <ItemStatusFilter />
+          <SearchPanel 
+          onSearchChange={this.onSearchChange} />
+          <ItemStatusFilter 
+          filter={filter}
+          onFilterChange={this.onFilterChange}
+          />
         </div>
-        <TodoList todos={todoData} 
+        <TodoList 
+          todos={visibleItems} 
           onDeleted={this.deleteItem} 
           onToggleImportant={this.onToggleImportant}
           onToggleDone={this.onToggleDone}  />
           <AddItemForm 
-          addItem={this.addItem}/>
+            addItem={this.addItem}
+            />
       </div>
     )
   }
